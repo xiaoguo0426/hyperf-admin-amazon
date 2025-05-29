216b570b8fcd827890026a462e988da45b73599c
Peter <peter.guo@prizmcn.com>
2025-05-29 11:19:19 +0800

    update

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -11,7 +11,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Kernel\Log\Log;
+use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\RequestMapping;
 
+#[Controller]
 class IndexController extends AbstractController
 {
     public function index()
@@ -26,5 +30,22 @@ class IndexController extends AbstractController
             'method' => $method,
             'message' => "Hello {$user}.",
         ];
+    }
+    #[RequestMapping(path: "test2",methods: "post")]
+    public function test2()
+    {
+        return [
+            'aa'=>11
+        ];
+    }
+
+    protected function test()
+    {
+        echo 1111;
+    }
+
+    private function foo()
+    {
+        echo 2222;
     }
 }
